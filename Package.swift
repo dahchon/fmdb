96c28f65055846cc86f3fc2d89080f7c47d0d96d
Michael Chong <daohanchong@gmail.com>
2024-03-23 10:05:49 -0600

    fix

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
         .target(
             name: "FMDB",
             dependencies: [],
-            path: "src",
+            path: "src/fmdb",
             publicHeadersPath: ".")
     ]
 )
